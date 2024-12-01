91af8de545b2957f83ad0d54962a3efb4b257e62
cactus1386 <cactusinjast@gmail.com>
2024-12-01 20:37:22 +0330

    filter set

--- core/products/filters.py
+++ core/products/filters.py
@@ -3,9 +3,9 @@ from .models import Product
 
 
 class ProductFilter(django_filters.FilterSet):
-    category = django_filters.CharFilter(
-        field_name='category__name', lookup_expr='icontains')
+    category_name = django_filters.CharFilter(
+        field_name='category_name', lookup_expr='icontains')
 
     class Meta:
         model = Product
-        fields = ['category', 'price']
+        fields = ['category_name', 'price']
