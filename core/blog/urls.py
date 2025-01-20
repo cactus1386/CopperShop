30a9040fdbbadfa052e701780e82736f4e02dc7f
cactus1386 <cactusinjast@gmail.com>
2025-01-20 19:50:06 +0330

    blogs ID bug

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import BlogView
+from .views import BlogView, BlogDetailView
 
 urlpatterns = [
     path('', BlogView.as_view(), name='blog-list'),
-    path('<int:pk>', BlogView.as_view(), name='blog-detail')
+    path('<int:pk>', BlogDetailView.as_view(), name='blog-detail')
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Blog
 from .serializers import BlogSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Create your views here.
 
 
-class BlogView(ListAPIView):
+class BlogView(ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = BlogSerializer
+    queryset = Blog.objects.all()
+
+
+class BlogDetailView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = BlogSerializer
     queryset = Blog.objects.all()
